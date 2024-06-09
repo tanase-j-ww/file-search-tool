8e07b95fda7fbdb8ebf2545a279ef792ba8301b1
tanase-j-ww <tanase.j@world-wing.com>
2024-06-09 22:29:18 +0900

    :sparkles: enable to fetch args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+        // コマンドライン引数を取得
+        let args: Vec<String> = env::args().collect();
+        if args.len() != 3 {
+            eprintln!("Usage: {} <directory> <pattern>", args[0]);
+            std::process::exit(1);
+        }
+        let directory = &args[1];
+        let pattern = &args[2];
+        
+        println!("directory: {} pattern: {}", directory, pattern);
 }
